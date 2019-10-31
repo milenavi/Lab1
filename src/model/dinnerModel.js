73bdd7c9300ec9a1bbedd79500d8140c77bbf721
Milena <milenavi@kth.se>
2019-10-31 19:57:54 +0100

    Lab1

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -64,6 +64,9 @@ class DinnerModel {
   //Returns the total price of the menu (price per serving of each dish multiplied by number of guests).
   getTotalMenuPrice() {
     //TODO Lab 1
+    return this.dinnerMenu.map(function(obj) { 
+      return obj.pricePerServing; 
+    }).reduce((a, b) => a + b, 0) * getNumberOfGuests()
   }
 
   //Adds the passed dish to the menu. If the dish of that type already exists on the menu
