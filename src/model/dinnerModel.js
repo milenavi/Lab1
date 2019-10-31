8a7f912b4b67584d6004082b970347f5f9057681
Milena <milenavi@kth.se>
2019-10-31 21:03:27 +0100

    Lab1

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -92,13 +92,13 @@ class DinnerModel {
   //Returns a dish of specific ID
   getDish(id) {
     /*for (let dish of this.dishes) {
-      if (dish.name === id) {
+      if (dish.id === id) {
         return dish;
       }
     }
     return undefined;
   }*/
-    return this.dishes.filter((obj) => { return (obj.id === id); } );
+    return this.dishes.filter((obj) => obj.id === id).shift()
 
       
       /*return this.dishes.filter(function(obj) { 
