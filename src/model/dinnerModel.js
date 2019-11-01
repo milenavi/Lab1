c248f5e9f2083beca230def342ce6e65c32517a0
Milena <milenavi@kth.se>
2019-11-01 10:04:24 +0100

    Lab 1

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -99,7 +99,7 @@ class DinnerModel {
           "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
         }
       }
-    ).then(response => response.json()).then(data => data).catch(console.error);
+    ).then(response => response.json()).then(data => data);
   }
 
   //Returns a dish of specific ID
@@ -119,7 +119,7 @@ class DinnerModel {
           "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
         }
       }
-    ).then(response => response.json()).then(data => data).catch(console.error);
+    ).then(response => response.json()).then(data => data);
   }
 }
 
